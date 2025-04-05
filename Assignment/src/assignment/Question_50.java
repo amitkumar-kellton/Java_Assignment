58ac49b7f3c21532e20ffa00a2457ae9d5d9d47e
amitkumar <amitkumar@localhost>
2025-04-06 02:14:55 +0530

    Done Question_50

--- Assignment/src/assignment/Question_50.java
+++ Assignment/src/assignment/Question_50.java
@@ -7,20 +7,27 @@ public class Question_50 {
 	public static void main(String[] args) {
 
 		System.out.println(
-				"\nQuestion 2- \n\n");
+				"\nQuestion 50- Count Elements Greater than a Given Value – Count how many numbers are greater than a specific value.\n\n");
 
 		Scanner scanner = new Scanner(System.in);
 
-		String stringToBeChecked = scanner.nextLine();
+		int numberToBeChecked = scanner.nextInt();
 
-		
+		int[] numbers = { 1, 2, 3, 2, 44, 55, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+		int count = 0;
+
 		int i = 0;
-		while (i >= 0) {
-
+		while (i < numbers.length) {
+			if (numbers[i] > numberToBeChecked) {
+				count++;
+			}
+			i++;
 		}
 
-
 		scanner.close();
+		System.out.println("The total number of elements in the array is: " + numbers.length);
+		System.out.println("The number of elements greater than " + numberToBeChecked + " is: " + count);
 	}
 
 }
