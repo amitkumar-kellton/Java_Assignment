3305b28fbc78bcbfc267dff2016a470a38552f25
amitkumar <amitkumar@localhost>
2025-04-06 22:31:13 +0530

    Done some Question from 50

--- Assignment/src/assignment/Question_43.java
+++ Assignment/src/assignment/Question_43.java
@@ -1,26 +1,47 @@
 package assignment;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Question_43 {
 
 	public static void main(String[] args) {
 
 		System.out.println(
-				"\nQuestion 2- \n\n");
+				"\nQuestion 43- Find the Maximum Occurring Element in a List – Identify the most frequent element.\n\n");
 
-		Scanner scanner = new Scanner(System.in);
+		List<Integer> integersList = new ArrayList<>();
 
-		String stringToBeChecked = scanner.nextLine();
+		integersList.add(2);
+		integersList.add(4);
+		integersList.add(4);
+		integersList.add(8);
+		integersList.add(10);
+		integersList.add(10);
 
-		
+		int max = 0;
+		int integer = 0;
+
 		int i = 0;
-		while (i >= 0) {
-
+		while (i < integersList.size()) {
+			int j = 0;
+			while (j < integersList.size()) {
+				if (integersList.contains(i)) {
+					integer = integersList.get(i);
+					max++;
+				}
+				j++;
+			}
+			i++;
 		}
+		
+		System.out.println("integer" + integer);
 
-
-		scanner.close();
+//		Iterator<Integer> itr = integersList.iterator();
+//		while(itr.hasNext()) {
+//			integersList.
+//		}
 	}
 
 }
--- Assignment/src/assignment/Question_47.java
+++ Assignment/src/assignment/Question_47.java
@@ -1,26 +1,39 @@
 package assignment;
 
-import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Question_47 {
 
 	public static void main(String[] args) {
 
-		System.out.println(
-				"\nQuestion 2- \n\n");
+		System.out.println("\nQuestion 47- Find the Union of Two Sets – Combine two sets.\n\n");
 
-		Scanner scanner = new Scanner(System.in);
+		Set<Integer> set1 = new HashSet<>();
+		Set<Integer> set2 = new HashSet<>();
+		Set<Integer> result = new HashSet<>();
 
-		String stringToBeChecked = scanner.nextLine();
+		set1.add(5);
+		set1.add(6);
+		set1.add(7);
+		set1.add(8);
+		set1.add(9);
 
-		
-		int i = 0;
-		while (i >= 0) {
+		System.out.println("\nSet 1: " + set1);
 
-		}
+		set2.add(10);
+		set2.add(11);
+		set2.add(12);
+		set2.add(13);
+		set2.add(14);
 
+		System.out.println("\nSet 2: " + set2);
 
-		scanner.close();
+		result.addAll(set1);
+		result.addAll(set2);
+
+		System.out.println("\nUnion of Two Sets: " + result);
+
 	}
 
 }
